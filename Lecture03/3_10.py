2acd181c4f9489d3563a6393a6a7be128eaddcc8
yujpark <ox8764@naver.com>
2023-04-22 21:41:44 +0900

    TIL 20230422

--- Lecture03/3_10.py
+++ Lecture03/3_10.py
@@ -6,7 +6,7 @@ for i in range(9):
     for j in range(9):
         check1 += a[i][j]
         check2 += a[j][i]
-    if (check1 != 45 or check2 != 45):
+    if (check1 != 45 or check2 != 45): #행의 합 & 열의 합 중 하나라도 조건을 만족하지 못한경우
         unique = False
 
 unique2 = True
@@ -15,8 +15,8 @@ for i in range(3):
         check3 = 0
         for k in range(3):
             for s in range(3):
-                check3 += a[3 * i + k][3 * j + s]
-        if check3 != 45:
+                check3 += a[3 * i + k][3 * j + s] 
+        if check3 != 45: #3x3짜리 사각형이 조건을 만족하지 못한 경우
             unique = False
     
 if unique1 and unique2:
