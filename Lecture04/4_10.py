3cba6e354266de2c941f1f74f91cabbdcb90d1d2
yujpark <ox8764@naver.com>
2023-04-29 06:32:25 +0900

    TIL 20230429

--- Lecture04/4_10.py
+++ Lecture04/4_10.py
@@ -1,12 +1,15 @@
 N = int(input())
 a = list(map(int, input().split()))
 b = []
+c = []
 while a:
     if a[0] > a[-1]:
         b.append(a[-1])
+        c.append('L')
         a.pop()
     else:
         b.append(a[0])
+        c.append('R')
         a.pop(0)
     if b[-1] >= a[0] and b[-1] >= a[-1]:
         break
