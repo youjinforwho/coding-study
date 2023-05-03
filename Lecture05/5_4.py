6fcc00c8c991e91a0d2dadf7a39daf31f8e8c018
yujpark <ox8764@naver.com>
2023-05-03 23:18:12 +0900

    TIL 20230503

---
+++ Lecture05/5_4.py
@@ -0,0 +1,19 @@
+exp = input()
+res = ''
+stack = []
+for i in exp:
+    if i == '*':
+        stack[-2] = stack[-2] * stack[-1]
+        stack.pop()
+    elif i == '/':
+        stack[-2] = stack[-2] // stack[-1]
+        stack.pop()
+    elif i == '+':
+        stack[-2] = stack[-2] + stack[-1]
+        stack.pop()
+    elif i == '-':
+        stack[-2] = stack[-2] - stack[-1]
+        stack.pop()
+    else:
+        stack.append(int(i))
+print(stack[-1])
