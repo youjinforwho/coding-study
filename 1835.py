6f74cd2bb982e826fbf3d53aeddf8dc776d2517e
yujpark <ox8764@naver.com>
2023-05-08 20:42:42 +0900

    move to baekjoon

--- 1835.py
+++
@@ -1,9 +0,0 @@
-from collections import deque
-N = int(input())
-dq = deque()
-dq.appendleft(N)
-for n in range(N - 1, 0, -1):
-    dq.appendleft(n)
-    for _ in range(n):
-        dq.appendleft(dq.pop())
-print(*dq)
