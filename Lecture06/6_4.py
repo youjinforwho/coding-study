7269e9fa437d71cc216dffc55f1fd08bc426d58b
yujpark <ox8764@naver.com>
2023-05-27 14:40:54 +0900

    TIL 20230527

---
+++ Lecture06/6_4.py
@@ -0,0 +1,15 @@
+import sys
+def DFS(L, sum):
+    if L == n:
+        if sum == (total - sum):
+            print("YES")
+            sys.exit(0)
+    else:
+        DFS(L + 1, sum + a[L])
+        DFS(L + 1, sum)
+
+if __name__ == "__main__":
+    n = int(input())
+    a = list(map(int, input().split()))
+    total = sum(a)
+    DFS(0, 0)
